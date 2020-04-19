3fe503c5585dcccc681d0f560794fea5446b1852
Jurgen <rjrooyen@gmail.com>
2020-04-20 11:08:22 +1200

    populateTables init

---
+++ populateTables.sql
@@ -0,0 +1,7 @@
+INSERT INTO director VALUES(1001, 'John', 'Smith', 0855512324, 100);
+
+INSERT INTO degree VALUES(200, 'Bachelor of Science', 360);
+
+INSERT INTO advisor VALUES(95, 'Adam', 'Steve', 'Tutor', 085554321, 102, 'Mathematics');
+
+INSERT INTO next_of_kin VALUES(03, "Jospeh", "Applegate", "Father", 035559767)
